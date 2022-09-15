afd6758134862d4483b74d1eb51b371ee67cf3cb
Soukaina Ouchen <souchen@e3r3p12.1337.ma>
2022-09-15 13:13:51 +0100

    Adding files and creating a table of some flights

---
+++ script.js
@@ -0,0 +1,56 @@
+let table = document.getElementById('body_table')
+
+let flight = [
+
+
+    {
+        time: "08:11",
+        destination: "Bamako (BKO)",
+        flight: "OX 203",
+        gate: "A 01",
+        remarks: "Landed-ON TIME[+]"
+
+    },
+
+    {
+        time: "13:20",
+        destination: "Landed-ON TIME[+]",
+        flight: "EY4219",
+        gate: "A 02",
+        remarks: "Landed-ON TIME[+]"
+
+    },
+    {
+        time: "20:21",
+        destination: "Malaga (AGP)",
+        flight: "OX 203",
+        gate: "A 08",
+        remarks: "Landed-Delayed"
+
+    },
+    {
+        time: "18:22",
+        destination: "Tangier (TNG)",
+        flight: "TK8322",
+        gate: "A 01",
+        remarks: "Landed-ON TIME[+]"
+
+    },
+    {
+        time: "22:30",
+        destination: "Conakry (CKY)",
+        flight: "IB8180",
+        gate: "A 08",
+        remarks: "Landed-Delayed"
+
+    },
+    {
+        time: "18:22",
+        destination: "Abu Dhabi (AUH)",
+        flight: "AT9283",
+        gate: "A 10",
+        remarks: "Landed-ON TIME[+]"
+
+    },
+
+]
